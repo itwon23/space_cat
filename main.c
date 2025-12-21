bd5847b9ba40f895ce2de6bfc81e8e3268377425
이창준 <cjlee1205@gmail.com>
2025-12-21 13:23:22 +0900

    Initialize global inventory in main function

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include "Intro.h"
 #include "SpaceCatGame.h"
+#include "Inventory.h"              
+Inventory global_inventory;        
 
 int main() {
+    init_inventory(&global_inventory);  
     show_title();
     show_prologue();
     run_space_cat_game();
