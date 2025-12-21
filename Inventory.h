3613ba77fdd3e19d657ae884a9ca8bb8147376d1
이창준 <cjlee1205@gmail.com>
2025-12-21 13:20:44 +0900

    Add inventory management function declarations
    Added function declarations for inventory management.

--- Inventory.h
+++ Inventory.h
@@ -9,9 +9,15 @@ typedef struct {
 typedef struct {
     Item food[20];
     int food_count;
-
     Item oxygen[20];
     int oxygen_count;
 } Inventory;
+
+
+void init_inventory(Inventory* inv);
+void add_food(Inventory* inv, const char* name, int recovery);
+void add_oxygen(Inventory* inv, int amount);
+int use_oxygen(Inventory* inv);
+void show_inventory(Inventory* inv);
 
 #endif
